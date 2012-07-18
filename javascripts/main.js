f6e1ea6be9722d42bca2cf91a1ef03bb17baf649
Halan Pinheiro <halan.pinheiro@gmail.com>
2012-07-18 13:37:27 -0300

    Added a try it

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
 console.log('This would be the main JS file.');
+
+$(function()
+{
+  $('#try-brid').change(function()
+  {
+    $self = $(this);
+    $.getJSON('http://brid-api.herokuapp.com/'+$self.val()+'?callback=?', function(r){
+      if(r.validation) $self.addClass('valid');
+      else $self.addClass('invalid');
+
+      $self.val(r.document);
+    })
+  }).focus(function()
+  {
+    $self = $(this);
+    $self.removeClass('valid').removeClass('invalid');
+  })
+});
