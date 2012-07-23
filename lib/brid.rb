b770ccbe8941e1ef23d79e120af20f675b9f85dc
Halan Pinheiro <halan.pinheiro@gmail.com>
2012-07-23 18:55:35 -0300

    Reorganizing files

--- lib/brid.rb
+++ lib/brid.rb
@@ -1,11 +1,29 @@
-['version.rb', 'mod.rb', 'mod10.rb', 'mod11.rb', 'generic_id.rb'].each do |filename|
-  require File.join(File.dirname(__FILE__), 'brid', filename)
+#requires
+
+root_dir = File.join(File.dirname(__FILE__), 'brid')
+
+require File.join(root_dir, 'version.rb')
+
+#mod algorithms
+[
+  'mod.rb',
+  'mod10.rb',
+  'mod11.rb'
+].each do |filename|
+  require File.join(root_dir, 'algorithms', filename)
 end
 
-['cpf.rb', 'cnpj.rb', 'titulo_eleitor.rb', 'pis.rb', 'cred_card.rb'].each do |filename|
-  require File.join(File.dirname(__FILE__), 'brid', 'ids', filename)
+#ids and documents
+[
+  'generic_id',
+  'cpf.rb',
+  'cnpj.rb',
+  'titulo_eleitor.rb',
+  'pis.rb',
+  'cred_card.rb'
+].each do |filename|
+  require File.join(root_dir, 'ids', filename)
 end
-
 
 require File.join(File.dirname(__FILE__), 'brid', 'detector.rb')
 
